070a7d6217f8012812cd7b826dd2b29d34a77520
Vafj <dev.vafj@gmail.com>
2021-02-16 03:36:49 -0300

    Cleaning sample.py file. You can use it in your test.

--- src/sample.py
+++ src/sample.py
@@ -2,42 +2,10 @@ from datetime import datetime
 
 
 def get_current_datetime(tz):
-    """
-    .:genpydoc:.
-    Write a description here.
-
-    Parameters:
-    ----------
-    param1: type
-        Description of param1.
-    paramN: type
-        Description of paramN.
-
-    Returns:
-    ----------
-    return_var: type
-        Description of return var.
-    """
     return datetime.now(tz=tz)
 
 
 def get_knowing_libs():
-    """
-    .:genpydoc:.
-    Write a description here.
-
-    Parameters:
-    ----------
-    param1: type
-        Description of param1.
-    paramN: type
-        Description of paramN.
-
-    Returns:
-    ----------
-    return_var: type
-        Description of return var.
-    """
     return ['numpy',
             'pandas',
             'pytest',
